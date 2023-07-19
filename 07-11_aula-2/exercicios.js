bc1a97f0e928327a94cb964b6f95310ecc88ad6d
gugaSouza-dev <gustavo_souza.dev@outlook.com>
2023-07-19 20:37:42 -0300

    adicionando o dowhile nos exercicios para que o programa rode até o usuario digitar o valor de escape

--- 07-11_aula-2/exercicios.js
+++ 07-11_aula-2/exercicios.js
@@ -17,7 +17,8 @@ function mensagemCor(cor, mensagem) {
 function numeroValidacao(mensagem) {
 	numero = parseFloat(prompt(mensagem))
 	if (isNaN(numero)){
-		console.log(mensagemCor(vermelho, "O valor informado não é um numero. Por favor, tente novamente."));
+		console.log(mensagemCor(vermelho,
+			"O valor informado não é um numero. Por favor, tente novamente."));
 		process.exit(-1)
 	}
 	return (numero)
@@ -50,166 +51,175 @@ function asciiMensagem() {
 
 asciiMensagem();
 
-let exercicio = numeroValidacao(roxo + mensagemCor(amarelo, "Informe o numero do exercicio (1 a 6): "))
+let exercicio = 1
 
-switch (exercicio) {
-	case 1:
-
-		/*
-		Exercício 1:
-		Faça um programa que receba o nome, cargo e salário de um
-		funcionário. Se o funcionário ganhar abaixo de 1000 reais, calcule o
-		salário acrescido de 10%. Ao final exiba o nome, o cargo e o salário
-		desse funcionário.
-		*/
-
-		console.log(verde + "\nCalcula acréscimo de 10% em valores menores que 1000\n");
-
-		let nome = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu nome: ") + roxo))
-		let cargo = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu cargo: ") + roxo))
-		let salarioBase = numeroValidacao(mensagemCor(amarelo, "Digite seu salario: ") + roxo)
-		let salarioAcrescido
-
-		if (salarioBase < 1000) {
-			salarioAcrescido = salarioBase + (salarioBase / 10)
-			console.log(ciano + `Nome: ` + roxo + `${nome}` + ciano + `.\nCargo: ` + roxo + ` ${cargo}.`)
-			console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}.\n` + ciano + `Salario acrescido: ` + roxo + ` ${parseFloat(salarioAcrescido).toFixed(2)}.`)
-		}
-		else{
-			console.log(ciano + `Nome: ` + roxo + `${nome}.\n` + ciano +`Cargo: ` + roxo + `${cargo}.`)
-			console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}\n` + ciano + `Seu salario não receberá o acrescimo.`);
-		}
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-
-	case 2:
-
-		/*
-		Exercício 2
-		Uma empresa decide dar aumento de 30% aos funcionários cujo
-		salário é inferior a 500 reais. Escreva um programa que receba o
-		salário de um funcionário e imprima o valor do salário reajustado ou
-		uma mensagem caso o funcionário não tenha direito a aumento.
-		*/
-
-		console.log(verde + "Checa se um valor receberá um acréscimo ou não. ");
-
-		let salario = numeroValidacao(amarelo + "Digite seu salario: ")
-
-		if (salario < 500) {
-			salario += salario / 30
-			console.log(ciano + "Seu salario foi reajustado para: " + roxo + salario.toFixed(2) + ".")
-		}
-		else
-			console.log(ciano + "Você não tem direito ao reajuste.")
-		
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-
-	case 3:
-
-		/*
-		Exercício 3
-		Faça um programa que receba um número inteiro do usuário e informe
-		se este número é positivo ou negativo.
-		*/
-
-		console.log(verde + "Checa se o seu numero é negativo, positivo ou neutro. ");
-
-		numero = numeroValidacao(amarelo + "Digite um numero: " + roxo)
-
-		if (numero < 0)
-			console.log(ciano + "Seu numero é negativo.")
-		else if (numero > 0)
-			console.log(ciano + "Seu numero é positivo")
-		else
-			console.log(ciano + "Zero é zero, parça (numero neutro).");
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-		
-	case 4:
-
-		/*
-		Exercício 4
-		Faça um programa que receba um número do usuário e informe
-		se este número é par ou ímpar.
-		*/
-
-		console.log(verde + "Checa se um numero é par ou impar. ");
-		numero = numeroValidacao(amarelo + "Digite um numero: " + roxo)
-
-		let restante = numero % 2
-
-		if (restante != 0)
-			console.log(ciano + "Seu numero é impar.")
-		else
-			console.log(ciano + "Seu numero é par")
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-
-	case 5:
-
-		/*
-		Exercício 5
-		Faça um programa receba dois valores e imprima qual é o maior
-		número digitado.
-		*/
-
-		console.log(verde + "Avalia qual é o maior entre dois numeros ");
-
-		let numeroUm = numeroValidacao(amarelo + "Digite o primeiro numero: " + roxo)
-		let numeroDois = numeroValidacao(amarelo + "Digite o segundo numero: " + roxo)
-
-		if (numeroUm > numeroDois)
-			console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroUm}.`)
-		else if
-			(numeroDois > numeroUm)
-			console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroDois}.`)
-		else
-			console.log(ciano + "Seus numeros são iguais")
-		console.log(mensagemCor(verde, "Fim."));
-
-		break;
-
-	case 6:
-
-		/*
-		Exercício 6
-		Faça um programa que receba 4 notas de um aluno, calcule e
-		imprima a média aritmética das notas e a mensagem de APROVADO
-		para média superior ou igual a 7,0 RECUPERAÇÃO para notas entre
-		5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
-		*/
-
-		function notaValidacao(mensagem) {
-			numero = parseFloat(prompt(mensagem))
-
-			if (isNaN(numero) || numero < 0 || numero > 10){
-				console.log(mensagemCor(vermelho, "A nota informada não foi apresentada corretamente. Por favor, tente outra vez."));
-				process.exit(-1)
+do {
+	console.log(mensagemCor(ciano, "\nPara sair digite: 0"));
+	exercicio = numeroValidacao(roxo + mensagemCor(amarelo, "Informe o numero do exercicio (1 a 6): ") + roxo)
+	switch (exercicio) {
+		case 1:
+	
+			/*
+			Exercício 1:
+			Faça um programa que receba o nome, cargo e salário de um
+			funcionário. Se o funcionário ganhar abaixo de 1000 reais, calcule o
+			salário acrescido de 10%. Ao final exiba o nome, o cargo e o salário
+			desse funcionário.
+			*/
+	
+			console.log(verde + "\nCalcula acréscimo de 10% em valores menores que 1000\n");
+	
+			let nome = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu nome: ") + roxo))
+			let cargo = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu cargo: ") + roxo))
+			let salarioBase = numeroValidacao(mensagemCor(amarelo, "Digite seu salario: ") + roxo)
+			let salarioAcrescido
+	
+			if (salarioBase < 1000) {
+				salarioAcrescido = salarioBase + (salarioBase / 10)
+				console.log(ciano + `Nome: ` + roxo + `${nome}` + ciano + `.\nCargo: ` + roxo + ` ${cargo}.`)
+				console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}.\n` +
+				ciano + `Salario acrescido: ` + roxo + ` ${parseFloat(salarioAcrescido).toFixed(2)}.`)
 			}
-			return (numero)
-		}
-
-		console.log(verde + "Calculadora de médias \nApresente os valores entre 0 e 10 ");
-
-		let notaUm = notaValidacao((amarelo + "Digite a primeira nota: " + roxo))
-		let notaDois = notaValidacao((amarelo + "Digite a segunda nota: " + roxo))
-		let notaTres = notaValidacao((amarelo + "Digite a terceira nota: " + roxo))
-		let notaQuatro = notaValidacao((amarelo + "Digite a quarta nota: " + roxo))
-		
-		let media = (notaUm + notaDois + notaTres + notaQuatro) / 4
-		
-		if (media >= 7)
-			console.log(ciano + `Média: ${media}. Aluno aprovado.`)
-		else if (media < 7 && media >= 5)
-			console.log(amarelo + `Média: ${media}. Aluno em recuperação.`)
-		else
-			console.log(vermelho + `Média: ${media}. Aluno reprovado.`)
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-
-	default:
-		console.log("Exercicio não encontrado. Tente um numero de 1 a 6")
-		break;
-}
+			else{
+				console.log(ciano + `Nome: ` + roxo + `${nome}.\n` + ciano +`Cargo: ` + roxo + `${cargo}.`)
+				console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}\n` + 
+				ciano + `Seu salario não receberá o acrescimo.`);
+			}
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+	
+		case 2:
+	
+			/*
+			Exercício 2
+			Uma empresa decide dar aumento de 30% aos funcionários cujo
+			salário é inferior a 500 reais. Escreva um programa que receba o
+			salário de um funcionário e imprima o valor do salário reajustado ou
+			uma mensagem caso o funcionário não tenha direito a aumento.
+			*/
+	
+			console.log(verde + "Checa se um valor receberá um acréscimo ou não. ");
+	
+			let salario = numeroValidacao(amarelo + "Digite seu salario: ")
+	
+			if (salario < 500) {
+				salario += salario / 30
+				console.log(ciano + "Seu salario foi reajustado para: " + roxo + salario.toFixed(2) + ".")
+			}
+			else
+				console.log(ciano + "Você não tem direito ao reajuste.")
+			
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+	
+		case 3:
+	
+			/*
+			Exercício 3
+			Faça um programa que receba um número inteiro do usuário e informe
+			se este número é positivo ou negativo.
+			*/
+	
+			console.log(verde + "Checa se o seu numero é negativo, positivo ou neutro. ");
+	
+			numero = numeroValidacao(amarelo + "Digite um numero: " + roxo)
+	
+			if (numero < 0)
+				console.log(ciano + "Seu numero é negativo.")
+			else if (numero > 0)
+				console.log(ciano + "Seu numero é positivo")
+			else
+				console.log(ciano + "Zero é zero, parça (numero neutro).");
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+			
+		case 4:
+	
+			/*
+			Exercício 4
+			Faça um programa que receba um número do usuário e informe
+			se este número é par ou ímpar.
+			*/
+	
+			console.log(verde + "Checa se um numero é par ou impar. ");
+			numero = numeroValidacao(amarelo + "Digite um numero: " + roxo)
+	
+			let restante = numero % 2
+	
+			if (restante != 0)
+				console.log(ciano + "Seu numero é impar.")
+			else
+				console.log(ciano + "Seu numero é par")
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+	
+		case 5:
+	
+			/*
+			Exercício 5
+			Faça um programa receba dois valores e imprima qual é o maior
+			número digitado.
+			*/
+	
+			console.log(verde + "Avalia qual é o maior entre dois numeros ");
+	
+			let numeroUm = numeroValidacao(amarelo + "Digite o primeiro numero: " + roxo)
+			let numeroDois = numeroValidacao(amarelo + "Digite o segundo numero: " + roxo)
+	
+			if (numeroUm > numeroDois)
+				console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroUm}.`)
+			else if
+				(numeroDois > numeroUm)
+				console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroDois}.`)
+			else
+				console.log(ciano + "Seus numeros são iguais")
+			console.log(mensagemCor(verde, "Fim."));
+	
+			break;
+	
+		case 6:
+	
+			/*
+			Exercício 6
+			Faça um programa que receba 4 notas de um aluno, calcule e
+			imprima a média aritmética das notas e a mensagem de APROVADO
+			para média superior ou igual a 7,0 RECUPERAÇÃO para notas entre
+			5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
+			*/
+	
+			function notaValidacao(mensagem) {
+				numero = parseFloat(prompt(mensagem))
+	
+				if (isNaN(numero) || numero < 0 || numero > 10){
+					console.log(mensagemCor(vermelho, 
+						"A nota informada não foi apresentada corretamente. Por favor, tente outra vez."));
+					process.exit(-1)
+				}
+				return (numero)
+			}
+	
+			console.log(verde + "Calculadora de médias \nApresente os valores entre 0 e 10 ");
+	
+			let notaUm = notaValidacao((amarelo + "Digite a primeira nota: " + roxo))
+			let notaDois = notaValidacao((amarelo + "Digite a segunda nota: " + roxo))
+			let notaTres = notaValidacao((amarelo + "Digite a terceira nota: " + roxo))
+			let notaQuatro = notaValidacao((amarelo + "Digite a quarta nota: " + roxo))
+			
+			let media = (notaUm + notaDois + notaTres + notaQuatro) / 4
+			
+			if (media >= 7)
+				console.log(ciano + `Média: ${media}. Aluno aprovado.`)
+			else if (media < 7 && media >= 5)
+				console.log(amarelo + `Média: ${media}. Aluno em recuperação.`)
+			else
+				console.log(vermelho + `Média: ${media}. Aluno reprovado.`)
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+	
+		default:
+			if (exercicio != 0)
+				console.log(mensagemCor(vermelho, "Exercicio não encontrado. Tente um numero de 1 a 10."))
+			break;
+	}
+	
+} while (exercicio != 0);
--- 07-11_aula-2/exercicios_loop.js
+++ 07-11_aula-2/exercicios_loop.js
@@ -49,148 +49,153 @@ function asciiMensagem() {
 
 asciiMensagem();
 
-let exercicio = numeroValidacao(roxo + mensagemCor(amarelo, 
-	"Informe o numero do exercicio (1 a 10): "))
 
+let exercicio = 1
 
-switch (exercicio) {
-	case 1:
-		/*
-		1. Imprimir números de 1 a 10 utilizando o loop for.
-		*/
-		console.log(verde + "Numeros de 1 a 10: ");
-		for (let i = 1; i <= 10; i++)
-			console.log(mensagemCor(azul, i));
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 2:
-		/*
-		2. Imprimir números pares de 1 a 10.
-		*/
-		console.log(verde + "Numeros pares de 1 a 10:");
-		for (let i = 0; i <= 10; i = i + 2)
-			console.log(mensagemCor(azul, i));
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 3:
-		/*
-		3. Faça um programa que imprima os números ímpares de 0 a 50.
-		*/
-		console.log(verde + "Numeros ímpares de 0 a 50:");
-		for (let i = 1; i <= 50; i = i + 2)
-			console.log(mensagemCor(azul, i));
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 4:
-		/*
-		4. Faça um programa que imprima na tela a tabuada de um número
-		qualquer escolhido pelo usuário até o 10.
-		*/
-		console.log(verde + "===== Tabuada =====");
-		let multiplicador = numeroValidacao(amarelo + 
-			"Informe o número que deseja ser multiplicado: ")
-		for (let i = 0; i <= 10; i++)
-			console.log(mensagemCor(roxo, `${multiplicador} * ${i} =`), 
-				(azul, `${multiplicador * i}`));
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 5:
-		/*
-		5. Imprimir os primeiros 10 números da sequência de Fibonacci.
-		*/
-		function fiboPosicao(fibo, posicao) {
-			console.log(mensagemCor(roxo, `Posição ${posicao}:\t`), mensagemCor(azul, `${fibo}.`))
-		}
-		console.log(verde + "Sequência de Febonacci:");
-		let t1 = 0, t2 = 1, fibonacci = t1 + t2
-		for (let i = 2; i <= 9; i++){
-			if (t1 == 0){
-				fiboPosicao(t1, 1);
-				fiboPosicao(t2, 2);
+do {
+	console.log(mensagemCor(ciano, "Para sair digite: 0"));
+	exercicio = numeroValidacao(roxo + mensagemCor(amarelo, 
+		"Informe o numero do exercicio (1 a 10): "))
+
+	switch (exercicio) {
+		case 1:
+			/*
+			1. Imprimir números de 1 a 10 utilizando o loop for.
+			*/
+			console.log(verde + "Numeros de 1 a 10: ");
+			for (let i = 1; i <= 10; i++)
+				console.log(mensagemCor(azul, i));
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 2:
+			/*
+			2. Imprimir números pares de 1 a 10.
+			*/
+			console.log(verde + "Numeros pares de 1 a 10:");
+			for (let i = 0; i <= 10; i = i + 2)
+				console.log(mensagemCor(azul, i));
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 3:
+			/*
+			3. Faça um programa que imprima os números ímpares de 0 a 50.
+			*/
+			console.log(verde + "Numeros ímpares de 0 a 50:");
+			for (let i = 1; i <= 50; i = i + 2)
+				console.log(mensagemCor(azul, i));
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 4:
+			/*
+			4. Faça um programa que imprima na tela a tabuada de um número
+			qualquer escolhido pelo usuário até o 10.
+			*/
+			console.log(verde + "===== Tabuada =====");
+			let multiplicador = numeroValidacao(amarelo + 
+				"Informe o número que deseja ser multiplicado: ")
+			for (let i = 0; i <= 10; i++)
+				console.log(mensagemCor(roxo, `${multiplicador} * ${i} =`), 
+					(azul, `${multiplicador * i}`));
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 5:
+			/*
+			5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+			*/
+			function fiboPosicao(fibo, posicao) {
+				console.log(mensagemCor(roxo, `Posição ${posicao}:\t`), mensagemCor(azul, `${fibo}.`))
 			}
-			fiboPosicao(fibonacci, i+1);
-			t1 = t2
-			t2 = fibonacci
-			fibonacci = t1 + t2
-		}
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 6:
-		/*
-		6. Imprimir números pares de 0 a 20 utilizando o loop while.
-		*/
-		console.log(verde + "Numeros pares de 0 a 20:");
-		numero = 0
-		while (numero <= 20) {
-			if (numero%2 == 0)
+			console.log(verde + "Sequência de Febonacci:");
+			let t1 = 0, t2 = 1, fibonacci = t1 + t2
+			for (let i = 2; i <= 9; i++){
+				if (t1 == 0){
+					fiboPosicao(t1, 1);
+					fiboPosicao(t2, 2);
+				}
+				fiboPosicao(fibonacci, i+1);
+				t1 = t2
+				t2 = fibonacci
+				fibonacci = t1 + t2
+			}
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 6:
+			/*
+			6. Imprimir números pares de 0 a 20 utilizando o loop while.
+			*/
+			console.log(verde + "Numeros pares de 0 a 20:");
+			numero = 0
+			while (numero <= 20) {
+				if (numero%2 == 0)
+					console.log(mensagemCor(azul, numero));
+				numero++
+			}
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 7:
+			/*
+			7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
+			while.
+			*/
+			console.log(mensagemCor(verde, "Soma dos numeros de 1 a 100"));
+			let soma = 0;
+			let contador = 0;
+			
+			while (contador <= 100) {
+				soma += contador
+				contador++;
+			}
+			console.log(mensagemCor(azul, soma));
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 8:
+			/*
+			8. Faça um programa que receba a idade do usuário, enquanto a
+			resposta do usuário for menor que 18 anos continue a solicitar a
+			idade e mostre uma mensagem quando a idade do usuário for
+			maior que 18.
+			*/
+			console.log(verde + "===== Validação de idade =====");
+			let idade = 0
+			while (idade < 18) {
+				idade = numeroValidacao(amarelo + "Digite sua idade: " + ciano)
+				if (idade < 18)
+					console.log(mensagemCor(vermelho, "Idade inválida."));
+			}
+			console.log(mensagemCor(verde, "Idade válida!"));
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 9:
+			/*
+			9. Faça um programa que receba um número do usuário e mostre
+			console a contagem regressiva até 0.
+			*/
+			console.log(verde + "===== Contagem regressiva =====");
+			numero = numeroValidacao(amarelo + "Informe seu numero: ")
+			while (numero >= 0) {
 				console.log(mensagemCor(azul, numero));
-			numero++
-		}
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 7:
-		/*
-		7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
-		while.
-		*/
-		console.log(mensagemCor(verde, "Soma dos numeros de 1 a 100"));
-		let soma = 0;
-		let contador = 0;
-		
-		while (contador <= 100) {
-			soma += contador
-			contador++;
-		}
-		console.log(mensagemCor(azul, soma));
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 8:
-		/*
-		8. Faça um programa que receba a idade do usuário, enquanto a
-		resposta do usuário for menor que 18 anos continue a solicitar a
-		idade e mostre uma mensagem quando a idade do usuário for
-		maior que 18.
-		*/
-		console.log(verde + "===== Validação de idade =====");
-		let idade = 0
-		while (idade < 18) {
-			idade = numeroValidacao(amarelo + "Digite sua idade: " + ciano)
-			if (idade < 18)
-				console.log(mensagemCor(vermelho, "Idade inválida."));
-		}
-		console.log(mensagemCor(verde, "Idade válida!"));
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 9:
-		/*
-		9. Faça um programa que receba um número do usuário e mostre
-		 console a contagem regressiva até 0.
-		*/
-		console.log(verde + "===== Contagem regressiva =====");
-		numero = numeroValidacao(amarelo + "Informe seu numero: ")
-		while (numero >= 0) {
-			console.log(mensagemCor(azul, numero));
-			numero--;
-		}
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	case 10:
-		/*
-		10. Faça um programa que receba um número do usuário e continue
-		a pedir o número se o numero for diferente de 0.
-		*/
-		console.log(verde + "===== Loop infinito de zeros =====");
-		numero = 1
-		while (numero != 0) {
-			numero = parseInt(prompt(amarelo + "Digite sua numero: "))
-			if (numero != 0)
-			console.log(mensagemCor(vermelho, "Numero inválido."));
-		}
-		console.log(mensagemCor(verde, "Valor valido!"));
-		console.log(mensagemCor(verde, "Fim."));
-		break;
-	default:
-		console.log(mensagemCor(vermelho, "Exercicio não encontrado. Tente um numero de 1 a 10."))
-		break;
-}
-
+				numero--;
+			}
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		case 10:
+			/*
+			10. Faça um programa que receba um número do usuário e continue
+			a pedir o número se o numero for diferente de 0.
+			*/
+			console.log(verde + "===== Loop infinito de zeros =====");
+			numero = 1
+			while (numero != 0) {
+				numero = parseInt(prompt(amarelo + "Digite sua numero: "))
+				if (numero != 0)
+				console.log(mensagemCor(vermelho, "Numero inválido."));
+			}
+			console.log(mensagemCor(verde, "Valor valido!"));
+			console.log(mensagemCor(verde, "Fim."));
+			break;
+		default:
+			if (exercicio != 0)
+				console.log(mensagemCor(vermelho, "Exercicio não encontrado. Tente um numero de 1 a 10."))
+			break;
+	}
+} while (exercicio != 0);
