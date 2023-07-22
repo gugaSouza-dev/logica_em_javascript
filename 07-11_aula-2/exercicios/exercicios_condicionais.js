60af2cef14d4f8c1dfc00ee2cbd2aaf9eb4e0707
Lidiane Lili <gustavo_souza.dev@outlook.com>
2023-07-22 10:41:34 -0300

    separando as funções em um arquivo e melhorando algumas condições

---
+++ 07-11_aula-2/exercicios/exercicios_condicionais.js
@@ -0,0 +1,213 @@
+import promptSync from "prompt-sync";
+import { mensagemCor, numeroValidacao } from "../main.js";
+
+//CORES
+import { vermelho, verde, amarelo,
+		azul, roxo, ciano } from "../main.js";
+
+const prompt = promptSync();
+
+function stringValidacao(string) {
+	if (!string) {
+		console.log(mensagemCor(vermelho, "Valor vazio não aceito."));
+		return (null)
+	}
+	return (string)
+}
+
+function notaValidacao(mensagem, numero) {
+	numero = parseFloat(prompt(mensagem))
+
+	if (isNaN(numero) || numero < 0 || numero > 10){
+		console.log(mensagemCor(vermelho, 
+			"A nota informada não foi apresentada corretamente. Por favor, tente outra vez."));
+		return (-1)
+	}
+	return (numero)
+}
+
+function mediaCalculator(notaUm, notaDois, notaTres, notaQuatro) {
+	let media
+	media = (notaUm + notaDois + notaTres + notaQuatro) / 4
+
+	if (media >= 7)
+		console.log(ciano + `Média: ${media}. Aluno aprovado.`)
+	else if (media < 7 && media >= 5)
+		console.log(ciano + `Média: ${media}.` + amarelo + ` Aluno em recuperação.`)
+	else
+		console.log(vermelho + `Média: ${media}. Aluno reprovado.`)
+}
+
+export function salarioAcrescimoDez(params) {
+	/*
+	Exercício 1:
+	Faça um programa que receba o nome, cargo e salário de um
+	funcionário. Se o funcionário ganhar abaixo de 1000 reais, calcule o
+	salário acrescido de 10%. Ao final exiba o nome, o cargo e o salário
+	desse funcionário.
+	*/
+	let nome
+	let cargo
+	let salarioBase
+	let salarioAcrescido
+
+	console.log(verde + "\nCalcula acréscimo de 10% em valores menores que 1000\n");
+
+	nome = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu nome: ") + roxo))
+	if (nome == null)
+		return (-1)
+	cargo = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu cargo: ") + roxo))
+	if (cargo == null)
+		return (-1)
+	salarioBase = numeroValidacao(prompt(
+		mensagemCor(amarelo, "Digite seu salario: ") + roxo))
+	if (isNaN(salarioBase))
+		return (-1)
+	salarioAcrescido
+
+	if (salarioBase < 1000) {
+		salarioAcrescido = salarioBase + (salarioBase / 10)
+		console.log(ciano + `Nome: ` + roxo + `${nome}` + ciano + `.\nCargo: ` + roxo + ` ${cargo}.`)
+		console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}.\n` +
+		ciano + `Salario acrescido: ` + roxo + ` ${parseFloat(salarioAcrescido).toFixed(2)}.`)
+	}
+	else{
+		console.log(ciano + `Nome: ` + roxo + `${nome}.\n` + ciano +`Cargo: ` + roxo + `${cargo}.`)
+		console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}\n` + 
+		ciano + `Seu salario não receberá o acrescimo.`);
+	}
+}
+
+export function salarioAcrescidoTrinta(params) {
+	/*
+	Exercício 2
+	Uma empresa decide dar aumento de 30% aos funcionários cujo
+	salário é inferior a 500 reais. Escreva um programa que receba o
+	salário de um funcionário e imprima o valor do salário reajustado ou
+	uma mensagem caso o funcionário não tenha direito a aumento.
+	*/
+	let salario
+
+	console.log(verde + "\nCheca se um valor receberá um acréscimo ou não. ");
+
+	salario = prompt(mensagemCor(amarelo, "Digite seu salario: ") + roxo)
+	salario = numeroValidacao(salario)
+	if (isNaN(salario))
+		return (-1)
+	if (salario < 500) {
+		salario += salario / 30
+		console.log(ciano + "Seu salario foi reajustado para: " + roxo + salario.toFixed(2) + ".")
+	}
+	else
+		console.log(ciano + "Você não tem direito ao reajuste.")
+
+}
+
+export function positivoOuNegativo(numero) {
+	/*
+	Exercício 3
+	Faça um programa que receba um número inteiro do usuário e informe
+	se este número é positivo ou negativo.
+	*/
+
+	console.log(verde + "\nCheca se o seu numero é negativo, positivo ou neutro. ");
+
+	numero = prompt(mensagemCor(amarelo, "Digite um numero: ") + roxo)
+	numero = numeroValidacao(numero)
+	if (isNaN(numero))
+		return (-1)
+
+	if (numero < 0)
+		console.log(ciano + "Seu numero é negativo.")
+	else if (numero > 0)
+		console.log(ciano + "Seu numero é positivo")
+	else
+		console.log(ciano + "Zero é zero, parça (numero neutro).");
+	return (-1)
+}
+
+export function imparOuPar(numero) {
+	/*
+	Exercício 4
+	Faça um programa que receba um número do usuário e informe
+	se este número é par ou ímpar.
+	*/
+	let restante = 0
+
+	console.log(verde + "\nCheca se um numero é par ou impar. ");
+	numero = prompt(mensagemCor(amarelo, "Digite um numero: ") + roxo)
+	numero = numeroValidacao(numero)
+	if (isNaN(numero))
+		return (-1)
+
+	restante = numero % 2
+
+	if (restante != 0)
+		console.log(ciano + "Seu numero é impar.")
+	else
+		console.log(ciano + "Seu numero é par")
+
+}
+
+export function maiorValor(params) {
+	/*
+	Exercício 5
+	Faça um programa receba dois valores e imprima qual é o maior
+	número digitado.
+	*/
+	let numeroUm
+	let numeroDois
+
+	console.log(verde + "\nAvalia qual é o maior entre dois numeros ");
+
+	numeroUm = prompt(mensagemCor(amarelo, "Digite o primeiro numero: ") + roxo)
+	numeroUm = numeroValidacao(numeroUm)
+	if (isNaN(numeroUm))
+		return (-1)
+	numeroDois = prompt(mensagemCor(amarelo, "Digite o segundo numero: ") + roxo)
+	numeroDois = numeroValidacao(numeroDois)
+	if (isNaN(numeroDois))
+		return (-1)
+	if (numeroUm > numeroDois)
+		console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroUm}.`)
+	else if
+		(numeroDois > numeroUm)
+		console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroDois}.`)
+	else
+		console.log(ciano + "Seus numeros são iguais")
+
+}
+
+
+
+export function media(numero) {
+	/*
+	Exercício 6
+	Faça um programa que receba 4 notas de um aluno, calcule e
+	imprima a média aritmética das notas e a mensagem de APROVADO
+	para média superior ou igual a 7,0 RECUPERAÇÃO para notas entre
+	5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
+	*/
+
+	let notaUm
+	let notaDois
+	let notaTres
+	let notaQuatro
+
+	console.log(verde + "\nCalculadora de médias \nApresente os valores entre 0 e 10 ");
+
+	notaUm = notaValidacao((amarelo + "Digite a primeira nota: " + roxo), numero)
+	if (notaUm == -1)
+		return(-1)
+	notaDois = notaValidacao((amarelo + "Digite a segunda nota: " + roxo), numero)
+	if (notaDois == -1)
+		return(-1)
+	notaTres = notaValidacao((amarelo + "Digite a terceira nota: " + roxo), numero)
+	if (notaTres == -1)
+		return(-1)
+	notaQuatro = notaValidacao((amarelo + "Digite a quarta nota: " + roxo), numero)
+	if (notaQuatro == -1)
+		return(-1)
+
+	mediaCalculator(notaUm, notaDois, notaTres, notaQuatro)
+}
--- 07-11_aula-2/menus/menu_condicionais.js
+++ 07-11_aula-2/menus/menu_condicionais.js
@@ -1,5 +1,7 @@
 import promptSync from "prompt-sync";
 import { mensagemCor, numeroValidacao, comandos } from "../main.js";
+import { imparOuPar, maiorValor, positivoOuNegativo,
+	 salarioAcrescidoTrinta, salarioAcrescimoDez, media } from "../exercicios/exercicios_condicionais.js";
 
 //CORES
 import { vermelho, verde, amarelo,
@@ -12,14 +14,6 @@ const prompt = promptSync();
 
 let exercicio
 
-function stringValidacao(string) {
-	if (!string) {
-		console.log(mensagemCor(vermelho, "Valor vazio não aceito."));
-		return (null)
-	}
-	return (string)
-}
-
 function opcoes() {
 	console.log(verde
 		+ "\n1 = Calcula acréscimo de 10% em valores menores que 1000"
@@ -29,7 +23,6 @@ function opcoes() {
 		+ "\n5 = Avalia qual é o maior entre dois numeros"
 		+ "\n6 = Calculadora de médias\n");
 }
-
 
 function asciiArtConditionals() {	
 	console.log(azul
@@ -41,11 +34,13 @@ function asciiArtConditionals() {
 		+ "\n  \\____\\___/|_| |_|\\__,_|_|\\__|_|\\___/|_| |_|\\__,_|_|___/")
 }
 
-
 export function conditionalsUm() {
+	let numero
+
 	asciiArtConditionals()
 	opcoes();
 	do {
+		numero = 0
 		comandos()
 		exercicio = prompt(mensagemCor(roxo, mensagemCor(amarelo,
 			"Informe o numero do exercicio (1 a 6): ")) + roxo)
@@ -56,201 +51,25 @@ export function conditionalsUm() {
 		{
 			switch (exercicio) {
 				case 1:
-			
-					/*
-					Exercício 1:
-					Faça um programa que receba o nome, cargo e salário de um
-					funcionário. Se o funcionário ganhar abaixo de 1000 reais, calcule o
-					salário acrescido de 10%. Ao final exiba o nome, o cargo e o salário
-					desse funcionário.
-					*/
-					let nome
-					let cargo
-					let salarioBase
-					let salarioAcrescido
-
-					console.log(verde + "\nCalcula acréscimo de 10% em valores menores que 1000\n");
-			
-					nome = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu nome: ") + roxo))
-					if (nome == null)
-						break;
-					cargo = stringValidacao(prompt(mensagemCor(amarelo, "Digite seu cargo: ") + roxo))
-					if (cargo == null)
-						break;
-					salarioBase = numeroValidacao(prompt(
-						mensagemCor(amarelo, "Digite seu salario: ") + roxo))
-					if (isNaN(salarioBase))
-						break;
-					salarioAcrescido
-			
-					if (salarioBase < 1000) {
-						salarioAcrescido = salarioBase + (salarioBase / 10)
-						console.log(ciano + `Nome: ` + roxo + `${nome}` + ciano + `.\nCargo: ` + roxo + ` ${cargo}.`)
-						console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}.\n` +
-						ciano + `Salario acrescido: ` + roxo + ` ${parseFloat(salarioAcrescido).toFixed(2)}.`)
-					}
-					else{
-						console.log(ciano + `Nome: ` + roxo + `${nome}.\n` + ciano +`Cargo: ` + roxo + `${cargo}.`)
-						console.log(ciano + `Salario base: ` + roxo + `${salarioBase.toFixed(2)}\n` + 
-						ciano + `Seu salario não receberá o acrescimo.`);
-					}
+					salarioAcrescimoDez()
 					break;
-			
 				case 2:
-			
-					/*
-					Exercício 2
-					Uma empresa decide dar aumento de 30% aos funcionários cujo
-					salário é inferior a 500 reais. Escreva um programa que receba o
-					salário de um funcionário e imprima o valor do salário reajustado ou
-					uma mensagem caso o funcionário não tenha direito a aumento.
-					*/
-					let salario
-
-					console.log(verde + "\nCheca se um valor receberá um acréscimo ou não. ");
-			
-					salario = prompt(mensagemCor(amarelo, "Digite seu salario: ") + roxo)
-					salario = numeroValidacao(salario)
-					if (isNaN(salario))
-						break;
-					if (salario < 500) {
-						salario += salario / 30
-						console.log(ciano + "Seu salario foi reajustado para: " + roxo + salario.toFixed(2) + ".")
-					}
-					else
-						console.log(ciano + "Você não tem direito ao reajuste.")
-					
+					salarioAcrescidoTrinta()
 					break;
 			
 				case 3:
-			
-					/*
-					Exercício 3
-					Faça um programa que receba um número inteiro do usuário e informe
-					se este número é positivo ou negativo.
-					*/
-			
-					console.log(verde + "\nCheca se o seu numero é negativo, positivo ou neutro. ");
-			
-					numero = prompt(mensagemCor(amarelo, "Digite um numero: ") + roxo)
-					numero = numeroValidacao(numero)
-					if (isNaN(numero))
-						break;
-
-					if (numero < 0)
-						console.log(ciano + "Seu numero é negativo.")
-					else if (numero > 0)
-						console.log(ciano + "Seu numero é positivo")
-					else
-						console.log(ciano + "Zero é zero, parça (numero neutro).");
+					positivoOuNegativo(numero)
 					break;
-					
 				case 4:
-			
-					/*
-					Exercício 4
-					Faça um programa que receba um número do usuário e informe
-					se este número é par ou ímpar.
-					*/
-					let restante
-
-					console.log(verde + "\nCheca se um numero é par ou impar. ");
-					numero = prompt(mensagemCor(amarelo, "Digite um numero: ") + roxo)
-					numero = numeroValidacao(numero)
-					if (isNaN(numero))
-						break;
-
-					restante = numero % 2
-			
-					if (restante != 0)
-						console.log(ciano + "Seu numero é impar.")
-					else
-						console.log(ciano + "Seu numero é par")
+					imparOuPar(numero)
 					break;
 			
 				case 5:
-			
-					/*
-					Exercício 5
-					Faça um programa receba dois valores e imprima qual é o maior
-					número digitado.
-					*/
-			
-					console.log(verde + "\nAvalia qual é o maior entre dois numeros ");
-			
-					numeroUm = prompt(mensagemCor(amarelo, "Digite o primeiro numero: ") + roxo)
-					numeroUm = numeroValidacao(numeroUm)
-					if (isNaN(numeroUm))
-						break
-					numeroDois = prompt(mensagemCor(amarelo, "Digite o segundo numero: ") + roxo)
-					numeroDois = numeroValidacao(numeroDois)
-					if (isNaN(numeroDois))
-						break
-					if (numeroUm > numeroDois)
-						console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroUm}.`)
-					else if
-						(numeroDois > numeroUm)
-						console.log(ciano + `Seu numero maior é: ` + roxo + `${numeroDois}.`)
-					else
-						console.log(ciano + "Seus numeros são iguais")
-			
+					maiorValor()
 					break;
 			
 				case 6:
-			
-					/*
-					Exercício 6
-					Faça um programa que receba 4 notas de um aluno, calcule e
-					imprima a média aritmética das notas e a mensagem de APROVADO
-					para média superior ou igual a 7,0 RECUPERAÇÃO para notas entre
-					5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
-					*/
-			
-					function notaValidacao(mensagem) {
-						numero = parseFloat(prompt(mensagem))
-			
-						if (isNaN(numero) || numero < 0 || numero > 10){
-							console.log(mensagemCor(vermelho, 
-								"A nota informada não foi apresentada corretamente. Por favor, tente outra vez."));
-							return (false)
-						}
-						return (numero)
-					}
-
-					function mediaCalculator(notaUm, notaDois, notaTres, notaQuatro) {
-						let media
-						media = (notaUm + notaDois + notaTres + notaQuatro) / 4
-					
-						if (media >= 7)
-							console.log(ciano + `Média: ${media}. Aluno aprovado.`)
-						else if (media < 7 && media >= 5)
-							console.log(ciano + `Média: ${media}.` + amarelo + ` Aluno em recuperação.`)
-						else
-							console.log(vermelho + `Média: ${media}. Aluno reprovado.`)
-					}
-
-					let notaUm
-					let notaDois
-					let notaTres
-					let notaQuatro
-			
-					console.log(verde + "\nCalculadora de médias \nApresente os valores entre 0 e 10 ");
-			
-					notaUm = notaValidacao((amarelo + "Digite a primeira nota: " + roxo))
-					if (!notaUm)
-						break;
-					notaDois = notaValidacao((amarelo + "Digite a segunda nota: " + roxo))
-					if (!notaDois)
-						break;
-					notaTres = notaValidacao((amarelo + "Digite a terceira nota: " + roxo))
-					if (!notaTres)
-						break;
-					notaQuatro = notaValidacao((amarelo + "Digite a quarta nota: " + roxo))
-					if (!notaQuatro)
-						break;
-
-					mediaCalculator(notaUm, notaDois, notaTres, notaQuatro)
-
+					media(numero)
 					break;
 			
 				default:
